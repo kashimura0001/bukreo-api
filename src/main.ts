496f0599729b15a73676850f7bd39acfe8d958f8
keisukekashimura <kashimura@refcome.com>
2021-02-10 09:52:03 +0900

    add firebase admin

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as admin from 'firebase-admin';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
+
+  admin.initializeApp({
+    credential: admin.credential.applicationDefault(),
+    databaseURL: 'https://bukure-e0f02.firebaseio.com',
+  });
 }
 bootstrap();
