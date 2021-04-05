00df4985fbb6cd8273b5f41eca4e18c9c74d1bd1
keisukekashimura <kashimura@refcome.com>
2021-04-05 10:28:21 +0900

    add comment

--- src/common/decorators/auth.decorator.ts
+++ src/common/decorators/auth.decorator.ts
@@ -1,6 +1,7 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { GqlExecutionContext } from '@nestjs/graphql';
 
+// userはauth.guard.tsで追加されています
 export const CurrentUser = createParamDecorator(
   (data: unknown, context: ExecutionContext) =>
     GqlExecutionContext.create(context).getContext().req.user,
