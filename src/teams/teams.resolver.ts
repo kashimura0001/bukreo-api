1fe7a2d691de502ee2605068ed1be028c5fc65d4
keisukekashimura <kashimura@refcome.com>
2021-04-27 22:14:12 +0900

    add members field

--- src/teams/teams.resolver.ts
+++ src/teams/teams.resolver.ts
@@ -1,35 +1,57 @@
-import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
+import {
+  Resolver,
+  Query,
+  Mutation,
+  Args,
+  ResolveField,
+  Parent,
+} from '@nestjs/graphql';
 import { TeamsService } from './teams.service';
 import { Team } from './entities/team.entity';
 import { CreateTeamInput } from './dto/create-team.input';
 import { UpdateTeamInput } from './dto/update-team.input';
 import { CurrentUser } from '../common/decorators/auth.decorator';
 import { User } from '../users/entities/user.entity';
+import { MembersService } from '../members/members.service';
 
 @Resolver(() => Team)
 export class TeamsResolver {
-  constructor(private readonly teamsService: TeamsService) {}
+  constructor(
+    private readonly teamsService: TeamsService,
+    private readonly membersService: MembersService,
+  ) {}
 
   @Mutation(() => Team)
-  createTeam(
+  async createTeam(
     @Args('input') input: CreateTeamInput,
     @CurrentUser() currentUser: User,
   ) {
     return this.teamsService.create(input, currentUser);
   }
 
-  @Query(() => Team)
-  team(@Args('id') id: string) {
-    return this.teamsService.findOne(id);
-  }
-
   @Mutation(() => Team)
-  updateTeam(@Args('input') input: UpdateTeamInput) {
+  async updateTeam(@Args('input') input: UpdateTeamInput) {
     return this.teamsService.update(input);
   }
 
   @Mutation(() => Team)
-  removeTeam(@Args('id') id: string) {
+  async removeTeam(@Args('id') id: string) {
     return this.teamsService.remove(id);
+  }
+
+  @Query(() => Team)
+  async team(@Args('id') id: string) {
+    return this.teamsService.findOne(id);
+  }
+
+  @ResolveField()
+  async members(@Parent() team: Team) {
+    return this.membersService.findAll({ teamId: team.id });
+  }
+
+  @ResolveField()
+  async invitations(@Parent() team: Team) {
+    // TODO あとで招待情報を取得できるようにする
+    return null;
   }
 }
--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,19 +1,25 @@
-import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import {
+  Resolver,
+  Query,
+  Mutation,
+  Args,
+  ResolveField,
+  Parent,
+} from '@nestjs/graphql';
 import { UsersService } from './users.service';
 import { User } from './entities/user.entity';
 import { CreateUserInput } from './dto/create-user.input';
 import { UpdateUserInput } from './dto/update-user.input';
 import { CurrentUser } from '../common/decorators/auth.decorator';
 import { DisableAuth } from '../common/decorators/disable-auth.decorator';
+import { MembersService } from '../members/members.service';
 
 @Resolver(() => User)
 export class UsersResolver {
-  constructor(private readonly usersService: UsersService) {}
-
-  @Query(() => User, { nullable: true })
-  async currentUser(@CurrentUser() currentUser: User) {
-    return currentUser;
-  }
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly membersService: MembersService,
+  ) {}
 
   @Mutation(() => User)
   @DisableAuth()
@@ -32,5 +38,21 @@ export class UsersResolver {
   @Mutation(() => User, { nullable: true })
   async removeUser(@CurrentUser() currentUser: User) {
     return await this.usersService.remove(currentUser.id);
+  }
+
+  @Query(() => User)
+  async currentUser(@CurrentUser() currentUser: User) {
+    return currentUser;
+  }
+
+  @ResolveField()
+  async members(@Parent() user: User) {
+    return this.membersService.findAll({ userId: user.id });
+  }
+
+  @ResolveField()
+  async invitations(@Parent() user: User) {
+    // TODO あとで招待情報を取得できるようにする
+    return null;
   }
 }
